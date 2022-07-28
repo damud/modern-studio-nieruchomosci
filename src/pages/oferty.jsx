470626a51c287690ac342292d84a90a9c59bef51
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-07-28 14:28:58 +0200

    Styled ThumbnailImage component

--- src/pages/oferty.jsx
+++ src/pages/oferty.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
 import { HighlightedHeading } from '../components/HighlightedHeading/HighlightedHeading';
 import { ContentWrapper } from '../components/ContentWrapper/ContentWrapper.styles';
 
+const ThumbnailWarpper = styled.div`
+	width: 100%;
+	height: 450px;
+	position: relative;
+	display: flex;
+	align-items: flex-end;
+`;
+
+const ThumbnailImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	object-position: left bottom;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: -1;
+`;
+
 export const Thumbnail = ({ imageSource }) => {
 	return (
-		<div>
-			<img src={imageSource} alt="" />
-
+		<ThumbnailWarpper>
+			<ThumbnailImage src={imageSource} alt="" />
 			<p>Polna 20</p>
 			<p>Nowoczesny apartament z klasą</p>
-		</div>
+		</ThumbnailWarpper>
 	);
 };
 
