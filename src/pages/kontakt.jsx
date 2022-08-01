bfd3680b3fda090e16fb5f20429cbec4b436335a
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-08-01 19:32:03 +0200

    Export styles in kontakt.styles.js file

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -1,65 +1,16 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
-import { ContentWrapper } from '../components/ContentWrapper/ContentWrapper.styles';
 import { HighlightedHeading } from '../components/HighlightedHeading/HighlightedHeading';
 import PhoneIcon from '../assets/icons-components/phone.svg';
 import {
 	ContactForm,
 	StyledButton,
 } from '../assets/styles/pages/homepage.styles';
-
-const ContactInfoWrapper = styled.section`
-	a {
-		color: black;
-		display: flex;
-		align-items: center;
-		margin: 5px 0 10px -7px;
-	}
-`;
-
-const ContactPhoto = styled.div`
-	display: none;
-
-	${({ theme }) => theme.mq.desktop} {
-		display: initial;
-		position: relative;
-		background-image: url('${({ imageSource }) => imageSource}');
-		background-repeat: no-repeat;
-		background-size: cover;
-		background-position: 0 80%;
-
-		${({ theme }) =>
-			theme.effect.corner({
-				position: 'bottomLeft',
-				size: '100px',
-				distance: '30px;',
-				color: theme.color.steel,
-			})};
-	}
-`;
-
-const ContactContentWrapper = styled(ContentWrapper)`
-	${({ theme }) => theme.mq.desktop} {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		grid-gap: 0 100px;
-
-		${ContactForm} {
-			width: 100%;
-			margin: 0;
-		}
-
-		${ContactForm}, ${ContactInfoWrapper} {
-			grid-column: 1 / 2;
-		}
-
-		${ContactPhoto} {
-			grid-column: 2 / 3;
-			grid-row: 1 / 3;
-		}
-	}
-`;
+import {
+	ContactContentWrapper,
+	ContactInfoWrapper,
+	ContactPhoto,
+} from '../assets/styles/pages/kontakt.styles';
 
 const Kontakt = ({ data }) => (
 	<ContactContentWrapper isSubpage>
