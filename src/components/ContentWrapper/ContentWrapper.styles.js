5997f7270179be32ef26845ae4637f3e375d61ec
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-09-17 20:14:38 +0200

    Fixed margin on homepage

--- src/components/ContentWrapper/ContentWrapper.styles.js
+++ src/components/ContentWrapper/ContentWrapper.styles.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const ContentWrapper = styled.div`
-	margin-top: ${({ isSubpage }) => (isSubpage ? '130px' : '0')};
+	margin-top: ${({ isSubpage }) => (isSubpage ? '130px' : '90px')};
 	padding: 0 20px;
 
 	${({ theme }) => theme.mq.desktop} {
 		max-width: 1920px;
-		margin: ${({ isSubpage }) => (isSubpage ? '180px' : '0')} auto 0 auto;
+		margin: ${({ isSubpage }) => (isSubpage ? '180px' : '80px')} auto 0 auto;
 		padding: 0 100px;
 	}
 `;
